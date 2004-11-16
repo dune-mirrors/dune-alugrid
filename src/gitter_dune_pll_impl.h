43f8acc92752b350924ae550a085b6bad0d6f91a
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2004-11-16 19:32:01 +0000

    logFile for each proc and new methods duneExchangeDynamicState.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/alugrid/trunk@45 0d966ed9-3843-0410-af09-ebfb50bd7c74

--- src/gitter_dune_pll_impl.h
+++ src/gitter_dune_pll_impl.h
@@ -10,11 +10,27 @@
 #include "gitter_dune_impl.h"
 #include "gitter_pll_impl.h"
 #include "gitter_pll_ldb.h"
+  
+//static ofstream logFile;
+static bool writeLogFile = false;
 
 class GitterDunePll : public GitterBasisPll , public GitterDuneBasis 
 {
 public:
-  GitterDunePll (const char * filename , MpAccessLocal &mp) : GitterBasisPll (filename,mp) {};
+  GitterDunePll (const char * filename , MpAccessLocal &mp) : GitterBasisPll (filename,mp) 
+  {
+    char logFileName [32];
+    sprintf(logFileName,"logfile.%d",mpAccess().myrank());
+    cerr << "open logfile = " << logFileName << "\n";
+
+    logFile.close();
+    logFile.open (logFileName);
+    logFile << "logfile of processor " << mpAccess().myrank() << "\n";
+  };
+
+  ~GitterDunePll () {
+    logFile.close();
+  }
 
   virtual bool duneAdapt () ; // done call notify and loadBalancer  
 
@@ -22,5 +38,14 @@ public:
   virtual bool duneLoadBalance (GatherScatterType & ) ; // call loadBalancer a
 
   virtual void duneRepartitionMacroGrid (LoadBalancer :: DataBase &, GatherScatterType & gs) ;
+  
+  void duneExchangeDynamicState ();
+  void duneExchangeDynamicState (GatherScatterType &);
+
+  IndexManagerType & indexManager(int codim)
+  {
+    return containerPll().indexManager(codim);
+  }
+
 };
 #endif
