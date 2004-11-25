657feb7a316c6082c2016de91fba3eca61847001
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2004-11-25 18:49:34 +0000

    removed duneAdaptm which is now in gitter_dune_pll_impl.*
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/alugrid/trunk@64 0d966ed9-3843-0410-af09-ebfb50bd7c74

--- src/gitter_pll_sti.cc
+++ src/gitter_pll_sti.cc
@@ -8,6 +8,9 @@
   
 /* $Id$
  * $Log$
+ * Revision 1.5  2004/11/25 18:49:34  robertk
+ * removed duneAdaptm which is now in gitter_dune_pll_impl.*
+ *
  * Revision 1.4  2004/11/02 18:55:30  robertk
  * Moved all changed with dune... to seperated gitter_dune_* files.
  *
@@ -356,6 +359,7 @@ pair < IteratorSTI < Gitter :: hface_STI > *, IteratorSTI < Gitter :: hface_STI 
 }
 
 bool GitterPll :: refine () {
+  cout << "refinepll \n";
   assert (debugOption (5) ? (cout << "**INFO GitterPll :: refine () " << endl, 1) : 1) ;
   const int nl = mpAccess ().nlinks (), start = clock () ;
   bool state = false ;
@@ -730,72 +734,6 @@ bool GitterPll :: adapt () {
   loadBalancerGridChangesNotify () ;
   return refined;
 }
-
-/*
-bool GitterPll :: duneAdapt () {
-  __STATIC_myrank = mpAccess ().myrank () ;
-  __STATIC_turn ++ ;
-  assert (debugOption (20) ? (cout << "**INFO GitterPll :: adapt ()" << endl, 1) : 1) ;
-  assert (! iterators_attached ()) ;
-  int start = clock () ;
-  bool refined = refine () ;
-  int lap = clock () ;
-  coarse () ;
-  int end = clock () ;
-  if (debugOption (1)) {
-    float u1 = (float)(lap - start)/(float)(CLOCKS_PER_SEC) ;
-    float u2 = (float)(end - lap)/(float)(CLOCKS_PER_SEC) ;
-    float u3 = (float)(end - start)/(float)(CLOCKS_PER_SEC) ;
-    cout << "**INFO GitterPll :: adapt () [ref (loops)|cse|all] " << u1 << " ("
-         << _refineLoops << ") " << u2 << " " << u3 << endl ;
-  }
-  notifyGridChanges () ;
-  return refined;
-}
-
-bool GitterPll :: duneLoadBalance () 
-{
-  loadBalancerGridChangesNotify () ;
-  return true;
-}
-
-bool GitterPll :: duneLoadBalance (GatherScatterType & gs) {
-  assert (debugOption (20) ? (cout << "**GitterPll :: loadBalancerGridChangesNotify () " << endl, 1) : 1) ;
-  const int start = clock (), me = mpAccess ().myrank (), np = mpAccess ().psize () ;
-  LoadBalancer :: DataBase db ;
-  {
-    AccessIterator < hface_STI > :: Handle w (containerPll ()) ;
-    for (w.first () ; ! w.done () ; w.next ()) w.item ().accessPllX ().ldbUpdateGraphEdge (db) ;
-  }
-  {
-    AccessIterator < helement_STI > :: Handle w (containerPll ()) ;
-    for (w.first () ; ! w.done () ; w.next ()) w.item ().accessPllX ().ldbUpdateGraphVertex (db) ;
-  }
-  bool neu = false ;
-  {
-  // Kriterium, wann eine Lastneuverteilung vorzunehmen ist:
-  // 
-  // load  - eigene ElementLast
-  // mean  - mittlere ElementLast
-  // nload - Lastverh"altnis
-
-  
-    double load = db.accVertexLoad () ;
-    vector < double > v (mpAccess ().gcollect (load)) ;
-    double mean = accumulate (v.begin (), v.end (), 0.0) / double (np) ;
-
-    for (vector < double > :: iterator i = v.begin () ; i != v.end () ; i ++)
-      neu |= (*i > mean ? (*i > (_ldbOver * mean) ? true : false) : (*i < (_ldbUnder * mean) ? true : false)) ;
-  }
-  if (neu) {
-    if (mpAccess ().gmax (_ldbMethod)) {
-      duneRepartitionMacroGrid (db, gs) ;
-      notifyMacroGridChanges () ;
-    }
-  }
-  return true;
-}
-*/
 
 void GitterPll :: MacroGitterPll :: fullIntegrityCheck (MpAccessLocal & mpa) {
   const int nl = mpa.nlinks (), me = mpa.myrank () ;
