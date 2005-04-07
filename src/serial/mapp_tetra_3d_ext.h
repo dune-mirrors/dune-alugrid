0dd3c0f6065bfd5927fd607b8b250443008dd3ad
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2005-04-07 12:26:35 +0000

    makes the normal calculation a bit faster;
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/alugrid/trunk@227 0d966ed9-3843-0410-af09-ebfb50bd7c74

--- src/serial/mapp_tetra_3d_ext.h
+++ src/serial/mapp_tetra_3d_ext.h
@@ -2,8 +2,11 @@
 #define __MAPP_TETRA_3D_EXT_HH__
 
 // BSGridVecType is defined in BSGrid interface to dune or in gitter_sti.hh 
-class BSGridLinearSurfaceMapping : public LinearSurfaceMapping 
+class BSGridLinearSurfaceMapping
 {
+protected:
+  double _n [3] ;
+  const double (&_p0)[3], (&_p1)[3], (&_p2)[3] ;
 public: 
   inline BSGridLinearSurfaceMapping 
     (const double (&)[3], const double (&)[3], const double (&)[3]);
@@ -15,8 +18,13 @@ public:
 
 inline BSGridLinearSurfaceMapping :: BSGridLinearSurfaceMapping (const double (&x0)[3], 
 const double (&x1)[3], const double (&x2)[3])
-  : LinearSurfaceMapping (x0,x1,x2)
+  //: LinearSurfaceMapping (x0,x1,x2)
+  : _p0 (x0), _p1 (x1), _p2 (x2)
 {
+  // copied from LinearSurfaceMapping, ist a bit faster 
+  _n[0] = -0.5 * ((_p1[1]-_p0[1]) *(_p2[2]-_p1[2]) - (_p2[1]-_p1[1]) *(_p1[2]-_p0[2])) ;
+  _n[1] = -0.5 * ((_p1[2]-_p0[2]) *(_p2[0]-_p1[0]) - (_p2[2]-_p1[2]) *(_p1[0]-_p0[0])) ;
+  _n[2] = -0.5 * ((_p1[0]-_p0[0]) *(_p2[1]-_p1[1]) - (_p2[0]-_p1[0]) *(_p1[1]-_p0[1])) ;
 }
 
 inline void BSGridLinearSurfaceMapping :: normal (double * normal) const
