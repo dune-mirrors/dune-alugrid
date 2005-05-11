bc3a092c313f3623b86297edc5ca884b0f7e2885
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2005-05-11 15:34:34 +0000

    removed internal boundary with ghost cells for a moment.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/alugrid/trunk@262 0d966ed9-3843-0410-af09-ebfb50bd7c74

--- src/duneinterface/gitter_dune_pll_mgb.cc
+++ src/duneinterface/gitter_dune_pll_mgb.cc
@@ -390,8 +390,10 @@ void DuneParallelGridMover :: finalize ()
   {for (hbndintMap_t :: iterator i = _hbnd3Int.begin () ; i != _hbnd3Int.end () ; i ++) {
     const Hbnd3IntStorage & p = * (Hbnd3IntStorage *) (*i).second ;
     if (p.first()->ref == 1) {
+      //hbndseg3_GEO * hb3 = myBuilder().insert_hbnd3 ( p.first(),p.second(),
+      //                  Gitter :: hbndseg_STI :: closure , p.getPoint() );
       hbndseg3_GEO * hb3 = myBuilder().insert_hbnd3 ( p.first(),p.second(),
-                        Gitter :: hbndseg_STI :: closure , p.getPoint() );
+                        Gitter :: hbndseg_STI :: closure );
       myBuilder ()._hbndseg3List.push_back (hb3) ;
     }
     delete (pair < hface3_GEO *, int > *)(*i).second ;
