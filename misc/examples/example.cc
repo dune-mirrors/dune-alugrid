b0e9e96a576144cdf9ad1e853e9d77b0840bddac
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2005-08-07 11:56:43 +0000

    little example how to ues grid. To be improved, but by others
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/alugrid/trunk@346 0d966ed9-3843-0410-af09-ebfb50bd7c74

---
+++ misc/examples/example.cc
@@ -0,0 +1,115 @@
+//***********************************************************************
+//
+//  Example program how to use ALU3d-Grid. 
+//  Author: Robert Kloefkorn 
+//
+//  This little program read one of the macrogrids and generates a grid. 
+//  The  grid is refined and coarsend again. 
+//
+//***********************************************************************
+#include <iostream>
+using namespace std;
+
+
+// include serial part of ALU3d-Grid 
+#include <alu3dgrid_serial.h>
+using namespace ALU3dGridSpace;
+
+// refine grid globally, i.e. mark all elements and then call adapt 
+void globalRefine(GitterBasisImpl* grid, int refcount) {
+    
+   for (int count=refcount ; count > 0; count--) {
+   cout << "Refine global: run " << refcount-count << endl;
+       {
+          // get LeafIterator which iterates over all leaf elements of the grid 
+          LeafIterator < Gitter::helement_STI > w (*grid) ;
+          
+          for (w->first () ; ! w->done () ; w->next ())
+          {
+            // mark element for refinement 
+            w->item ().tagForGlobalRefinement ();
+          }
+       }
+       // adapt grid 
+       grid->adapt ();
+
+       // print size of grid 
+       grid->printsize () ;
+   }
+}
+
+// coarse grid globally, i.e. mark all elements for coarsening 
+// and then call adapt 
+void globalCoarsening(GitterBasisImpl* grid, int refcount) {
+    
+   for (int count=refcount ; count > 0; count--) {
+   cout << "Global Coarsening: run " << refcount-count << endl;
+       {
+          // get LeafIterator which iterates over all leaf elements of the grid 
+          LeafIterator < Gitter::helement_STI > w (*grid) ;
+          
+          for (w->first () ; ! w->done () ; w->next ())
+          {
+            // mark elements for coarsening  
+            w->item ().resetRefinementRequest () ;
+          }
+       }
+       // adapt grid 
+       grid->adapt ();
+
+       // print size of grid 
+       grid->printsize () ;
+   }
+}
+
+// perform walk over elements of a certain level  
+void levelwalk(GitterBasisImpl* grid, int level) {
+   typedef Insert <AccessIterator <
+     Gitter::helement_STI>::Handle, 
+     TreeIterator <Gitter :: helement_STI, any_has_level <Gitter::helement_STI> > > 
+       LevelIterator;
+
+   LevelIterator it (grid->container(), level);
+   int i = 0;
+   for (it.first(); !it.done(); it.next()) 
+   {
+      cout << "Element " << it.item().getIndex() << " has " << i++ << " as level index " << endl;
+   }
+   cout << endl;
+}
+
+
+// exmaple on read grid, refine global and print again 
+int main (int argc, const char ** argv, const char ** envp) 
+{
+   int mxl = 3; 
+   if (argc < 2) 
+   {
+      cout << "usage: ./"<< argv[0] << " <macro grid> <opt: level> \n";
+      abort();
+   }
+
+   if (argc < 3)
+   {
+      cout << "Default level = "<< mxl << "choosen! \n";
+   }
+
+   std::string macroname( argv[1] );
+
+   cout << "\n-----------------------------------------------\n";
+   cout << "read macro grid from < " << macroname << " > !" << endl;
+   cout << "-----------------------------------------------\n";
+
+   GitterBasisImpl grid(macroname.c_str());
+   cout << "Grid generated! \n";
+   grid.printsize(); 
+   cout << "---------------------------------------------\n";
+   
+
+   globalRefine(&grid, mxl);
+   levelwalk(&grid, mxl);
+   globalCoarsening(&grid, mxl);
+
+   return 0;
+}
+
