5649f487710bd91b717e99f8ce2e0a4ccb431f7b
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2006-08-28 09:15:06 +0000

    changed comment to english.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/alugrid/trunk@666 0d966ed9-3843-0410-af09-ebfb50bd7c74

--- src/parallel/metis.c
+++ src/parallel/metis.c
@@ -17,20 +17,28 @@
   #include <iostream.h>
 #endif
 
-static const char metmess [] = 	"**INFO Aus lizenzrechtlichen Gr\"unden steht die Bibliothek\n"
-				"       metis 3.0 nicht im Rahmen der Quelldateien auf diesem\n"
-				"       Datentr\"ager zur Verf\"ugung. Weitere Hinweise sind in\n"
-				"       der Datei " __FILE__ " enthalten." ;
+static const char metmess [] = 	"**INFO Due to license reasons the library METIS is\n"
+				"       not part of the ALUGrid library distribution. \n"
+				"       To use this feature get a copy of the METIS library \n"
+        "       (see http://www-users.cs.umn.edu/~karypis/metis/metis/ )\n"
+        "       and re-configure the ALUGrid library with the \n"
+        "       --with-metis=PATH_TO_METIS option, \n"
+        "       or choose another Graph partitioning method. \n"
+        "       Exiting program, bye! \n";
 
+// dummy METIS_PartGraphKway, when METIS library is missing 
 void METIS_PartGraphKway(int *,idxtype *,idxtype *,idxtype *,idxtype *,int *,int *,int *,int *,int *,idxtype *) {
+  cerr << "**ERROR The use of METIS_PartGraphKway is not supported, when the METIS library is missing! in: " << __FILE__ << " line: " << __LINE__ << "\n";
   cerr << metmess << endl ;
-  abort();
+  exit(1); 
   return ;
 }
 
+// dummy METIS_PartGraphRecursive, when METIS library is missing 
 void METIS_PartGraphRecursive(int *,idxtype *,idxtype *,idxtype *,idxtype *,int *,int *,int *,int *,int *,idxtype *) {
+  cerr << "**ERROR The use of METIS_PartGraphRecursive is not supported, when the METIS library is missing! " << __FILE__ << " line: " << __LINE__ << "\n";
   cerr << metmess << endl ;
-  abort();
+  exit(1);
   return ;
 }
 
