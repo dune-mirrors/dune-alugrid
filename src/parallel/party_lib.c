b4f9d84f959422f7b52ee6eb87348429fbe522c9
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2006-08-28 09:23:24 +0000

    changed comment to english.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/alugrid/trunk@667 0d966ed9-3843-0410-af09-ebfb50bd7c74

--- src/parallel/party_lib.c
+++ src/parallel/party_lib.c
@@ -15,44 +15,60 @@
   #include <iostream.h>
 #endif
 
-static const char parmess [] = 	"**INFO Aus lizenzrechtlichen Gr\"unden steht die Bibliothek\n"
-				"       PARTY 1.1 nicht im Rahmen der Quelldateien auf diesem\n"
-				"       Datentr\"ager zur Verf\"ugung. Weitere Hinweise sind in\n"
-				"       der Datei " __FILE__ " enthalten." ;
+static const char parmess [] = 	"**INFO Due to license reasons the library PARTY is\n"
+				"       not part of the ALUGrid library distribution. \n"
+				"       To use this feature get a copy of the PARTY library \n"
+        "       (see http://wwwcs.upb.de/fachbereich/AG/monien/RESEARCH/PART/party.html)\n"
+        "       and re-configure the ALUGrid library with the \n"
+        "       --with-party=PATH_TO_PARTY option, \n"
+        "       or choose another Graph partitioning method. \n"
+        "       Exiting program, bye! \n";
 
-
+// dummy method, when library missing        
 int global_linear (int,float *,int,float,int *) {
+  cerr << "**ERROR The use of global_linear is not supported, when the PARTY library is missing! in: " << __FILE__ << " line: " << __LINE__ << "\n";
   cerr << parmess << endl ;
-  abort();
+  exit(1);
   return 0 ;
 }
 
+// dummy method, when library missing        
 int global_scattered (int,float *,int,float,int *) {
+  cerr << "**ERROR The use of global_scattered is not supported, when the PARTY library is missing! in: " << __FILE__ << " line: " << __LINE__ << "\n";
   cerr << parmess << endl ;
-  abort();
+  exit(1);
   return 0 ;
 }
 
+// dummy method, when library missing        
 int global_gain (int,float *,int *,int *,int *,int,float,int *) {
+  cerr << "**ERROR The use of global_gain is not supported, when the PARTY library is missing! in: " << __FILE__ << " line: " << __LINE__ << "\n";
   cerr << parmess << endl ;
-  abort();
+  exit(1);
   return 0 ;
 }
 
+// dummy method, when library missing        
 int global_farhat (int,float *,int *,int *,int *,int,float,int *) {
+  cerr << "**ERROR The use of global_farhat is not supported, when the PARTY library is missing! in: " << __FILE__ << " line: " << __LINE__ << "\n";
   cerr << parmess << endl ;
-  abort();
+  exit(1);
   return 0 ;
 }
 
+// dummy method, when library missing        
 int local_kl (int,float *,int *,int *,int *,int,float, int *,int) {
+  cerr << "**ERROR The use of local_kl is not supported, when the PARTY library is missing! in: " << __FILE__ << " line: " << __LINE__ << "\n";
   cerr << parmess << endl ;
-  abort();
+  exit(1);
   return 0 ;
 }
 
+// dummy method, when library missing        
 int local_hs (int n, float *vertex_w, int *edge_p, int *edge, int *edge_w,int p, float add_bal, int *part,int Output) {
+  cerr << "**ERROR The use of local_hs is not supported, when the PARTY library is missing! in: " << __FILE__ << " line: " << __LINE__ << "\n";
   cerr << parmess << endl ;
+  exit(1);
   return 0 ;
 }
 
