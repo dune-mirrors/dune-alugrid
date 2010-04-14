37924d7cd2ac53d89d85bad60e62c01acf5a0073
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2010-04-14 12:24:31 +0000

    compile object file here.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/alugrid/trunk@1239 0d966ed9-3843-0410-af09-ebfb50bd7c74

---
+++ src/starmpi.c
@@ -0,0 +1,15 @@
+#if HAVE_STARMPI
+#error
+// compile STAR-MPI object file
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+// include STAR-MPI sources to compile object file
+#include <star-mpi.c>
+
+#if defined(__cplusplus)
+} 
+#endif
+
+#endif
