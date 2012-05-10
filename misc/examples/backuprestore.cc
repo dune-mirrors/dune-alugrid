9f8754bd7af7064ca95190628acac2ea34f3e8c6
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2012-05-10 07:38:50 +0000

    small change
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/alugrid/trunk@1585 0d966ed9-3843-0410-af09-ebfb50bd7c74

--- misc/examples/backuprestore.cc
+++ misc/examples/backuprestore.cc
@@ -27,6 +27,32 @@ using namespace std;
 
 using namespace ALUGridSpace;
 
+template <class GitterType, class element_t> 
+void checkElement( GitterType& grid, element_t& elem ) 
+{
+  if( elem.type() == tetra )
+  {
+    typedef typename GitterType :: Objects :: tetra_IMPL tetra_IMPL ;
+    // mark element for refinement 
+    tetra_IMPL& item = ((tetra_IMPL &) elem);
+
+    for( int i=0; i<4; ++i ) 
+    {
+      assert( item.myneighbour( i ).first->isRealObject() );
+    }
+  }
+  else 
+  {
+    typedef typename GitterType :: Objects :: hexa_IMPL hexa_IMPL ;
+    // mark element for refinement 
+    hexa_IMPL& item = ((hexa_IMPL &) elem);
+
+    for( int i=0; i<6; ++i ) 
+    {
+      assert( item.myneighbour( i ).first->isRealObject() );
+    }
+  }
+}
 
 // refine grid globally, i.e. mark all elements and then call adapt 
 template <class GitterType>
@@ -43,6 +69,7 @@ void globalRefine(GitterType& grid, int refcount)
         {
           // mark element for refinement 
           w->item ().tagForGlobalRefinement ();
+          checkElement( grid, w->item() );
         }
      }
 
@@ -69,6 +96,7 @@ void globalCoarsening(GitterType& grid, int refcount) {
        
        for (w->first () ; ! w->done () ; w->next ())
        {
+         checkElement( grid, w->item() );
          // mark elements for coarsening  
          w->item ().tagForGlobalCoarsening() ;
        }
@@ -87,6 +115,8 @@ void globalCoarsening(GitterType& grid, int refcount) {
 int main (int argc, char ** argv, const char ** envp) 
 {
   MPI_Init(&argc,&argv);
+  int rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
   int mxl = 0; 
   const char* filename = 0 ;
@@ -110,26 +140,36 @@ int main (int argc, char ** argv, const char ** envp)
   cout << "read macro grid from < " << macroname << " > !" << endl;
   cout << "-----------------------------------------------\n";
 
+  std::stringstream backupname ; 
+  backupname << "file." << rank ;
   {
-#ifdef PARALLEL
-    MpAccessMPI a (MPI_COMM_WORLD);
-    GitterDunePll grid(macroname.c_str(),a);
-#else 
-    GitterDuneImpl grid(macroname.c_str());
-#endif
+//#ifdef PARALLEL
+//    MpAccessMPI a (MPI_COMM_WORLD);
+//    GitterDunePll grid(macroname.c_str(),a);
+//#else 
+    std::ifstream infile( macroname.c_str());
+    GitterDuneImpl grid1( infile );
+    infile.close();
+
+    std::ifstream infile2( macroname.c_str());
+    GitterDuneImpl grid2( infile2 );
+    infile2.close();
+//#endif
+    GitterDuneImpl& grid = grid2; 
    
     cout << "Grid generated! \n";
     globalRefine(grid, mxl);
     cout << "---------------------------------------------\n";
 
-    std::ofstream file( "file.out" );
+    std::ofstream file( backupname.str().c_str() );
     grid.duneBackup( file );
     file.close();
 
+    globalCoarsening(grid, mxl);
   }
 
   {
-    std::ifstream file( "file.out" );
+    std::ifstream file( backupname.str().c_str() );
     // read grid from file 
 #ifdef PARALLEL
     MpAccessMPI a (MPI_COMM_WORLD);
@@ -138,7 +178,11 @@ int main (int argc, char ** argv, const char ** envp)
     GitterDuneImpl grid( file );
 #endif
     grid.printsize();
-    grid.duneRestore( file );
+
+    //grid.duneRestore( file );
+    globalRefine(grid, mxl);
+    // adapt grid 
+
     cout << "Grid restored!" << std::endl;
     grid.printsize();
     cout << "---------------------------------------------\n";
