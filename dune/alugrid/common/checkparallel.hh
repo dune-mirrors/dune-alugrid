1c3be964f56bb4f545ae94f9740b0f7cc1fd76dd
Andreas Dedner <a.s.dedner@warwick.ac.uk>
2013-01-17 15:19:45 +0000

    do not need to test if alugrid was build with mpi

--- dune/alugrid/common/checkparallel.hh
+++ dune/alugrid/common/checkparallel.hh
@@ -1,21 +1,10 @@
 #ifndef DUNE_ALUGRID_CHECKPARALLEL_HH
 #define DUNE_ALUGRID_CHECKPARALLEL_HH
 
-#include <dune/alugrid/src/parallel/alugrid_defineparallel.h>
-
 #if HAVE_MPI
-  // if this variable is defined, 
-  // // then parallel version of ALUGrid is compiled
-  #if ALU3DGRID_BUILD_FOR_PARALLEL == 0
-    #warning "The ALUGrid-library wasn't compiled for parallel usage. Reconfigure\
- using the MPI compiler script or compile Dune without the MPI support!\
- Defaulting to serial ALUGrid!"
-    #define ALU3DGRID_PARALLEL 0
-  #else 
-    #define ALU3DGRID_PARALLEL 1
-  #endif
-#else  // #if HAVE_MPI
+  #define ALU3DGRID_PARALLEL 1
+#else 
   #define ALU3DGRID_PARALLEL 0
-#endif // #else  // #if HAVE_MPI
+#endif
 
 #endif // #ifndef DUNE_ALUGRID_CHECKPARALLEL_HH
