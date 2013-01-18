120566c61f2178567ecdc3e446a6842f3a42c12c
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2013-01-18 11:24:32 +0200

    put mpAccess.cc into a (new) parallel lib (not using any MPI)

--- dune/alugrid/src/parallel/mpAccess.cc
+++ dune/alugrid/src/parallel/mpAccess.cc
@@ -1,45 +1,51 @@
+#include <algorithm>
+#include <iostream>
+
 #include "mpAccess.h"
 
-void MpAccessLocal :: printLinkage (ostream & out) const {
-  out << "  MpAccessLocal :: printLinkage () " << myrank () << " -> " ;
-  { for (map < int, int, less < int > > :: const_iterator i = _linkage.begin () ; 
-  	i != _linkage.end () ; i ++) out << (*i).first << " " ; }
-  out << endl ;
-  return ;
+void MpAccessLocal::printLinkage ( std::ostream &out ) const
+{
+  out << "  MpAccessLocal::printLinkage() " << myrank () << " -> ";
+  for (std::map < int, int >::const_iterator i = _linkage.begin (); i != _linkage.end(); ++i )
+    out << (*i).first << " ";
+  out << std::endl;
 }
 
-int MpAccessLocal :: insertRequestSymetric (set < int, less < int > > req) 
+int MpAccessLocal::insertRequestSymetric ( std::set< int > req )
 {
-  const int me = myrank () ;
-  req.erase (me) ;
-  vector < int > out ;
-  {for (set < int, less < int > > :: const_iterator i = req.begin () ; i != req.end () ; i ++ )
-    if (_linkage.find (*i) == _linkage.end ()) out.push_back (*i) ; }
-  vector < vector < int > > in = gcollect (out) ;
-  { for (vector < int > :: const_iterator i = out.begin () ; i != out.end () ; i ++ )
+  const int me = myrank ();
+  req.erase (me);
+  std::vector< int > out;
+  {for (std::set< int >::const_iterator i = req.begin (); i != req.end (); i ++ )
+    if (_linkage.find (*i) == _linkage.end ()) out.push_back (*i); }
+  std::vector< std::vector< int > > in = gcollect (out);
+  { for (std::vector< int >::const_iterator i = out.begin (); i != out.end (); i ++ )
     if (_linkage.find (*i) == _linkage.end ()) {
-      int n = _linkage.size () ;
-      _linkage [*i] = n ;
+      int n = _linkage.size ();
+      _linkage [*i] = n;
     }
   }
-  int cnt = 0 ;
-  { for (int i = 0 ; i < psize () ; i ++ )
-    if (in [i].end() != find (in [i].begin(), in [i].end(), me)) {
-      assert (i != me) ;
-      if (_linkage.find (i) == _linkage.end ()) {
-        int n = _linkage.size () ;
-        _linkage [i] = n ;
-        cnt ++ ; 
+  int cnt = 0;
+  for( int i = 0; i < psize(); ++i )
+  {
+    if( std::find (in[ i ].begin(), in[ i ].end(), me) != in[ i ].end()  )
+    {
+      assert (i != me);
+      if (_linkage.find (i) == _linkage.end ())
+      {
+        int n = _linkage.size ();
+        _linkage [i] = n;
+        cnt ++; 
       }
     }
   }
 
   // setup dest vector 
   {
-    _dest.resize( _linkage.size () ) ;
-    for (map < int, int, less < int > > :: const_iterator i = _linkage.begin () ; 
-  	  i != _linkage.end () ; i ++) _dest[(*i).second] = (*i).first ;
+    _dest.resize( _linkage.size () );
+    for( std::map< int, int >::const_iterator i = _linkage.begin (); i != _linkage.end (); ++i )
+      _dest[(*i).second] = (*i).first;
   }
 
-  return cnt ;
+  return cnt;
 }
