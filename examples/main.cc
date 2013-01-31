6455032c592101be84323f3ec45952881eaa946b
Robert Kloefkorn <robertk@ucar.edu>
2013-01-31 16:17:48 -0700

    small change.

--- examples/main.cc
+++ examples/main.cc
@@ -195,13 +195,13 @@ void method ( const ModelType &model, int startLevel, int maxLevel )
       const size_t maxDofsPerElem = (elements > 0) ? (solution.size()/elements) : 0;
       // write times to run file 
       diagnostics.write( time, dt,                   // time and time step
-                       elements,                     // number of elements
-                       maxDofsPerElem,               // number of dofs per element (max)
-                       solveTime,                    // time for operator evaluation 
-                       commTime + adaptation.communicationTime(), // communication time  
-                       adaptation.adaptationTime(),  // time for adaptation 
-                       adaptation.loadBalanceTime(), // time for load balance
-                       overallTimer.elapsed());      // time step overall time
+                         elements,                     // number of elements
+                         maxDofsPerElem,               // number of dofs per element (max)
+                         solveTime,                    // time for operator evaluation 
+                         commTime + adaptation.communicationTime(), // communication time  
+                         adaptation.adaptationTime(),  // time for adaptation 
+                         adaptation.loadBalanceTime(), // time for load balance
+                         overallTimer.elapsed());      // time step overall time
     }
   }           
   /* output final result */
