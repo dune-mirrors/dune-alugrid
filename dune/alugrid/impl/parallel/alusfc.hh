1f01ebbc754c18d9f274bae255a1abb7150c9b79
Robert Kloefkorn <robertk@ucar.edu>
2013-02-09 22:40:18 -0700

    provide balanced sfc.

--- dune/alugrid/impl/parallel/alusfc.hh
+++ dune/alugrid/impl/parallel/alusfc.hh
@@ -35,7 +35,10 @@ namespace ALUGridMETIS
     assert( std::abs( meanLoad - mpa.gmax( meanLoad ) ) < 1e-8 );
 
     // round the average load to get a threshold value 
-    double meanThreshold = std::round( meanLoad );
+    double meanThreshold = std::round ( meanLoad ) ;
+
+    // vector of loads 
+    std::vector< int > offsets( nPart+1, -1 );
 
     bool emptyProc = true ;
     bool readjust = false ;
@@ -44,25 +47,88 @@ namespace ALUGridMETIS
       double load = 0;
       idxtype rank = 0;
       const idxtype lastRank = nPart - 1 ;
-      for( idxtype i = 0; i < nCells; ++i )
+      for( idxtype elem = 0; elem < nCells; ++elem )
       {
-        double nextLoad = load + weights[ i ] ;
+        double nextLoad = load + weights[ elem ] ;
         if( load > 0 && ( nextLoad > meanThreshold ) )           
         {
           // only increase rank if not already the last rank 
-          if( rank < lastRank ) ++rank ;
+          if( rank < lastRank ) 
+          {
+            ++rank ;
+            // remember first occurance of rank 
+            offsets[ rank ] = elem;
+          }
           load = 0;
         }
 
-        part[ i ] = rank ;
-        load += weights[ i ];
+        part[ elem ] = rank ;
+        load += weights[ elem ];
       }
+      offsets[ nPart ] = nCells;
 
       // the last element should belong to the last proc,
       // otherwise the partition is not balanced 
       if( part[ nCells-1 ] == lastRank )
       {
-        emptyProc = false ;
+        emptyProc  = false ;
+
+        // vector of loads 
+        std::vector< double > loads( nPart, 0.0 );
+
+        // check load balance 
+        for( idxtype i = 0; i<nCells; ++i )
+        {
+          loads[ part[ i ] ] += weights[ i ];
+        }
+
+        double minLoad = loads[ 0 ];
+        double maxLoad = loads[ 0 ];
+
+        // check load balance 
+        for( int i = 1; i < nPart; ++ i ) 
+        {
+          minLoad = std::min( minLoad, loads[ i ] );
+          maxLoad = std::max( maxLoad, loads[ i ] );
+        }
+
+        // if unbalanced partition detected 
+        // the situation is most likely that the last proc has 
+        // to many elements assigned 
+        if( maxLoad/meanThreshold > 1.02 || minLoad/meanThreshold < 0.97 ) 
+        {
+          const double upperBound = 1.01 * meanThreshold;
+          // backwards sweep 
+          for( int i = nPart-1; i > 0; -- i ) 
+          {
+            while ( loads[ i ] > upperBound && offsets[ i ] < (offsets[ i+1 ] - 1) )
+            {
+              // put the firt element of this partition to the 
+              // previous proc 
+              --part[ offsets[ i ] ] ;
+              loads[ i ]   -= weights[ offsets[ i ] ];
+              loads[ i-1 ] += weights[ offsets[ i ] ];
+              ++offsets[ i ];
+              assert( part[ offsets[ i ] ] >= 0 );
+            }
+          }
+
+          // backwards sweep 
+          for( int i = 0; i<nPart-1; ++i ) 
+          {
+            while ( loads[ i ] > upperBound && offsets[ i ] < (offsets[ i+1 ] - 1) )
+            {
+              // put the firt element of this partition to the 
+              // previous proc 
+              const int lastIndex = (offsets[ i+1 ]-1);
+              ++part[ lastIndex ] ;
+              loads[ i ]   -= weights[ lastIndex ];
+              loads[ i-1 ] += weights[ lastIndex ];
+              --offsets[ i+1 ];
+              assert( part[ lastIndex ] < nPart );
+            }
+          }
+        }
       }
       else 
       {
@@ -82,8 +148,20 @@ namespace ALUGridMETIS
         loads[ part[ i ] ] += weights[ i ];
       }
 
+      double minLoad = loads[ 0 ];
+      double maxLoad = loads[ 0 ];
+
+      // check load balance 
+      for( int i = 0; i < nPart; ++ i ) 
+      {
+        minLoad = std::min( minLoad, loads[ i ] );
+        maxLoad = std::max( maxLoad, loads[ i ] );
+      }
+
       std::cout << "OrgLoad: mean = " << std::round( meanLoad ) << std::endl;
       std::cout << "Loads: mean = " << meanThreshold << std::endl;
+      std::cout << "Loads: min = " << minLoad << "  max = " << maxLoad << std::endl;
+      std::cout << "unbal counter = " << unbalancedCounter << std::endl;
       for( idxtype i = 0; i<nPart; ++i )
       {
         std::cout << "P[ " << i << " ] = " << loads[ i ] << std::endl;
