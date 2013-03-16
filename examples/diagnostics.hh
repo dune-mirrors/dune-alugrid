ff9bbb8eef33d64f50b5546cf18842f4009fffb0
Robert Kloefkorn <robertk@ucar.edu>
2013-03-15 18:24:03 -0600

    small change.

--- examples/diagnostics.hh
+++ examples/diagnostics.hh
@@ -3,11 +3,18 @@
 
 #include "threadmanager.hh"
 
+#define PRINT_IDENTIFICATION_TIMES
+
 #ifdef PRINT_IDENTIFICATION_TIMES 
 namespace ALUGrid { 
   extern double identU2;
   extern double identU3;
   extern double identU4;
+
+  extern double ldbTimerU2;
+  extern double ldbTimerU3;
+  extern double ldbTimerU4;
+  extern double ldbTimerU5;
 }
 #endif
 
@@ -142,21 +149,29 @@ namespace Dune {
 #ifdef PRINT_IDENTIFICATION_TIMES 
     void printIdentificationTimes() const
     {
-      std::vector<double> times(3);
-      times[ 0 ] = ::ALUGrid::identU2;
-      times[ 1 ] = ::ALUGrid::identU3;
-      times[ 2 ] = ::ALUGrid::identU4;
+      std::vector<double> times;
+
+      times.push_back( ::ALUGrid::identU2 );
+      times.push_back( ::ALUGrid::identU3 );
+      times.push_back( ::ALUGrid::identU4 );
+
+      times.push_back( ::ALUGrid::ldbTimerU2 );
+      times.push_back( ::ALUGrid::ldbTimerU3 );
+      times.push_back( ::ALUGrid::ldbTimerU4 );
+      times.push_back( ::ALUGrid::ldbTimerU5 );
+
+      const int timerSize = times.size();
 
       std::vector<double> maxTimes( times );
       std::vector<double> minTimes( times );
 
       // sum, max, and min for all procs 
-      comm_.max( &maxTimes[ 0 ], 3 );
-      comm_.min( &minTimes[ 0 ], 3 );
+      comm_.max( &maxTimes[ 0 ], timerSize );
+      comm_.min( &minTimes[ 0 ], timerSize );
 
       if( comm_.rank() == 0 )  
       {
-        for(int i=0; i<3; ++i )
+        for(int i=0; i<timerSize; ++i )
         {
           std::cout << "U" << i+2 << " max: " << maxTimes[ i ] << "  min: " << minTimes[ i ] << std::endl;
         }
