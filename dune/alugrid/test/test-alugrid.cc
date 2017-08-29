e799227dcd61c51e9dd8dc58b53defa9157954f2
Robert K <robertk@posteo.org>
2017-08-29 15:38:18 +0200

    [bugfix] reset filenames to previous values.
    Former-commit-id: cf80e07daa1aa70482655a799fde1d1f66e7efcf

--- dune/alugrid/test/test-alugrid.cc
+++ dune/alugrid/test/test-alugrid.cc
@@ -520,7 +520,6 @@ void checkALUSerial(GridType & grid, int mxl = 2)
   //if( checkTwist )
   //  checkTwists( grid.leafGridView(), NoMapTwist() );
 
-  /*
   for(int i=0; i<mxl; i++)
   {
     grid.globalRefine( Dune::DGFGridInfo< GridType >::refineStepsForHalf() );
@@ -531,10 +530,10 @@ void checkALUSerial(GridType & grid, int mxl = 2)
     // if( checkTwist )
     //  checkTwists( grid.leafGridView(), NoMapTwist() );
   }
-  */
 
   // check also non-conform grids
   makeNonConfGrid(grid,0,1);
+
   writeFile( grid.levelGridView( 0 ), "level" );
   writeFile( grid.leafGridView(), "leaf" );
 
@@ -859,7 +858,7 @@ int main (int argc , char **argv) {
         if( newfilename )
           filename = newfilename;
         else
-          filename = "./dgf/unitcube3.dgf";
+          filename = "./dgf/simplex-testgrid-3-3.dgf";
 
         typedef Dune::ALUGrid< 3, 3, Dune::cube, Dune::nonconforming > GridType;
         Dune::GridPtr< GridType > gridPtr( filename );
@@ -868,13 +867,11 @@ int main (int argc , char **argv) {
 
         checkCapabilities< false >( grid );
 
-        /*
         {
           std::cout << "Check serial grid" << std::endl;
           checkALUSerial(grid,
                          (mysize == 1) ? 1 : 0 );
         }
-        */
 
         // perform parallel check only when more then one proc
         if(mysize > 1)
